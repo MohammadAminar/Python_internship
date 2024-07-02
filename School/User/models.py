5bcea82245447c9b44780ff5f4f805abff9879aa
Amin Arabpour <maadmad3080364465@gmail.com>
2024-07-02 17:14:57 +0330

    edit models.py

--- School/User/models.py
+++ School/User/models.py
@@ -1,5 +1,5 @@
-from abc import ABC, abstractmethod
-from django.db import models
+from django.db import models # type: ignore
+
 
 class User(models.Model):
     Id = models.AutoField(primary_key=True)
@@ -7,36 +7,36 @@ class User(models.Model):
     Password = models.CharField(max_length=50)
     First_name = models.CharField(max_length=50)
     Last_name = models.CharField(max_length=50)
-    Phone = models.IntegerField(max_length=50)
-    International_code = models.IntegerField(max_length=50)
+    Phone = models.IntegerField()
+    International_code = models.IntegerField()
 
 class Teacher(User, models.Model):
     CHOICES = [
         ('True', 'بله'),
         ('False', 'خیر'),
-    ],
+    ]
     User_Id = models.AutoField(primary_key=True)
-    Code_vezarat_olom = models.IntegerField(max_length=50)
+    Code_vezarat_olom = models.IntegerField()
     Work_experience = models.BooleanField(default=False, choices=CHOICES)
     Education = models.CharField(max_length=50)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_query_name='Teacher')
 
 class Student(User, models.Model):
     DEBT_STATUS_CHOICES = [
         ('SUSPENDED', 'معلق شده'),
         ('PAID', 'پرداخت شده'),
         ('UNPAID', 'پرداخت نشده'),
-    ],
+    ]
     User_Id = models.AutoField(primary_key=True)
     Education_level = models.CharField(max_length=50)
     Debt_status = models.CharField(max_length=50, choices=DEBT_STATUS_CHOICES)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_query_name='Student')
 
 class Parent(User, models.Model):
     User_Id = models.AutoField(primary_key=True)
-    Number_of_children = models.IntegerField(max_length=50)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Number_of_children = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_query_name='Parent')
 
 class Manager(User, models.Model):
     User_Id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_query_name='Manager')
